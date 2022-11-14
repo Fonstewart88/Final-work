93b08017fe91ca8d4a868c7e6be16479c032696c
Fonstewart88 <stewart88@list.ru>
2022-11-14 16:12:08 +0300

    Вывод второго массива

--- Program.cs
+++ Program.cs
@@ -28,11 +28,19 @@ void FillArray(string[] arrayX, string[] arrayY)
     }
 }
 
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
 
 
 string[] array1 = new string[9] {"22", "55555", "333", "4444", "A", "BB", "CCC", "DDDD", "1"};
 int L = Array2Length(array1);
 string[] array2 = new string[L];
+FillArray(array1,array2);
+PrintArray(array2);
 
-
-Console.Write(L);
