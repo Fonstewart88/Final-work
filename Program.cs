664b33a500c69b023b6b9dcf9cf036e20bb4bd7d
Fonstewart88 <stewart88@list.ru>
2022-11-14 15:57:44 +0300

    Определено количество элементов длиной 3 и ниже - длина второго массива

--- Program.cs
+++ Program.cs
@@ -4,3 +4,19 @@
 
 string[] array1 = new string[9] {"22", "55555", "333", "4444", "A", "BB", "CCC", "DDDD", "1"};
 
+int Array2Length(string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        count++;
+        }
+    }
+    return count;
+}    
+
+int L = Array2Length(array1);
+
+Console.Write(L);
